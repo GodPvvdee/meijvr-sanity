e11375108ceda6d59ce592d16b8ea21bb2309ec1
pvvdee <56915962+GodPvvdee@users.noreply.github.com>
2022-01-11 11:40:00 +0800

    kekkee

---
+++ schemas/author.js
@@ -0,0 +1,55 @@
+export default {
+  name: 'product',
+  title: 'Product',
+  type: 'document',
+  fields: [
+    {
+      name: 'name',
+      title: 'Name',
+      type: 'string',
+    },
+    {
+      name: 'slug',
+      title: 'Slug',
+      type: 'slug',
+      options: {
+        source: 'name',
+        maxLength: 96,
+      },
+    },
+    {
+      name: 'image',
+      title: 'Image',
+      type: 'image',
+      options: {
+        hotspot: true,
+      },
+    },
+    {
+      name: 'images',
+      title: 'Images',
+      type: 'array',
+      of: [{type: 'image'}]
+   
+    },
+    {
+      name: 'bio',
+      title: 'Bio',
+      type: 'array',
+      of: [
+        {
+          title: 'Block',
+          type: 'block',
+          styles: [{title: 'Normal', value: 'normal'}],
+          lists: [],
+        },
+      ],
+    },
+  ],
+  preview: {
+    select: {
+      title: 'name',
+      media: 'image',
+    },
+  },
+}
---
+++ schemas/shop.js
@@ -0,0 +1,48 @@
+export default {
+    name: 'shop',
+    title: 'Shop',
+    type: 'document',
+    fields: [
+      {
+        name: 'name',
+        title: 'Name',
+        type: 'string',
+      },
+      {
+        name: 'slug',
+        title: 'Slug',
+        type: 'slug',
+        options: {
+          source: 'name',
+          maxLength: 96,
+        },
+      },
+      {
+        name: 'images',
+        title: 'Images',
+        type: 'array',
+        of: [{type: 'image'}]
+     
+      },
+      {
+        name: 'bio',
+        title: 'Bio',
+        type: 'array',
+        of: [
+          {
+            title: 'Block',
+            type: 'block',
+            styles: [{title: 'Normal', value: 'normal'}],
+            lists: [],
+          },
+        ],
+      },
+    ],
+    preview: {
+      select: {
+        title: 'name',
+        media: 'image',
+      },
+    },
+  }
+  
